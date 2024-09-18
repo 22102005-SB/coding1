46404451c9cf9aef9cdc7ba6a3613bc3f39c3b27
22102005-SB <sarabcarneiro2005@gmail.com>
2024-09-18 18:01:53 +0000

    criando algoritimo boletim

---
+++ aula.js
@@ -0,0 +1,12 @@
+//este codigo foi para trein de variaves
+
+/*var rotulo = "natalya" 
+var rotulo = 12345
+console.log(rotulo)
+*/
+
+
+var x1 = 85
+var x2 = 5
+var some = x1 + x2
+console.log("o resultado da soma e: "+some)
---
+++ aula2.js
@@ -0,0 +1,5 @@
+const nota1 = 9;
+const nota2 = 6;
+const nota3 = 4;
+const media = (nota1 + nota2 + nota3)/3
+console.log(`a media do aluno é: ${media}`)
