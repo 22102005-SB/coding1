5d8070dc3169b7e36963c2b54dec1b1d41ed3a32
22102005-SB <sarabcarneiro2005@gmail.com>
2024-10-01 19:02:07 +0000

    atividade de code

---
+++ atividade.js
@@ -0,0 +1,6 @@
+const nome = "sara"
+let idade = 18
+console.log(`òla meu nome e ${nome} e tenho ${idade} de idade`)
+
+const cidade = "jaboatao dos guararapes"
+console.log(`ola meu nome e ${nome} e moro em ${cidade}`)
